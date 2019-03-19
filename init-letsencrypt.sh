c4845f5f1a067377df91f073cc5b6a02e2b86ccd
sky-labs-237 <40956582+sky-labs-237@users.noreply.github.com>
2019-03-19 21:00:22 +0100

    Update init-letsencrypt.sh

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -3,7 +3,7 @@
 domains=(apps.sky-labs.io yako.apps.sky-labs.io)
 rsa_key_size=4096
 data_path="./data/certbot"
-email="yves@sky-labs.io" # Adding a valid address is strongly recommended
+email="yves.bimai@sky-labs.io" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
